1b5a4ff74dfaa2576102fe704210299c9b67a3b7
Rhuan Chaves <rhuantac@gmail.com>
2020-08-04 10:09:29 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tab from '@material-ui/core/Tab';
+import { AppBar, Tabs, makeStyles, Container, Typography, Button } from '@material-ui/core';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney'; 
+import Box from '@material-ui/core/Box'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow:1,
+    textAlign: 'center',
+    backgroundColor: theme.palette.background.paper
+  },
+  mainDiv: {
+    marginTop: 'auto',
+		position: 'absolute',
+		width: '30vw',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%,-50%)'
+  },
 
+  insertCoinButton: {
+    marginBottom: '10px',  
+    position: 'absolute',    
+    width: '90%',
+    height: '10%',
+    bottom: '0',
+    left: '0',
+    right: '0',    
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    backgroundColor: theme.palette.success.main,
+    '&:hover': {
+      backgroundColor: theme.palette.success.dark,
+    },
+  },
+
+  spendCoinButton: {
+    marginBottom: '10px',  
+    position: 'absolute',    
+    width: '90%',
+    height: '10%',
+    bottom: '0',
+    left: '0',
+    right: '0',    
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  }
+}))
 function App() {
+  const classes = useStyles();
+  const [currentTab, setCurrentTab] = React.useState(0);
+  const [fichas, setFichas] = React.useState(parseInt(localStorage.getItem('fichas'))|| 0)
+  const onChangeTab = (event, newValue) => {
+    setCurrentTab(newValue);
+  }
+
+  const increaseFichas = () => {
+    const newFichas = fichas + 1;
+    localStorage.setItem('fichas', newFichas.toString())
+    setFichas(newFichas)
+    
+  }
+
+  const decreaseFichas = () => {
+    if(fichas <= 0) {
+      alert('Vai treinar, seu sedentário kkk')
+      return;
+    }
+    const newFichas = fichas - 1;
+    localStorage.setItem('fichas', newFichas.toString())
+    setFichas(newFichas)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+     <AppBar position="static">
+      <Tabs value={currentTab} onChange={onChangeTab} variant="fullWidth">
+        <Tab icon={<FitnessCenterIcon/>} label="Comprar"/>
+        <Tab icon={<SportsEsportsIcon/>} label="Pagar"/>
+       </Tabs>       
+     </AppBar>
+
+     <Box hidden={currentTab !== 0}>
+        <div className={classes.mainDiv}>
+          <Typography variant="h1" component="h1">
+            {fichas}
+          </Typography>
+          <Typography>
+            Fichas
+          </Typography>          
+        </div> 
+        <Button onClick={increaseFichas} className={classes.insertCoinButton} size="large" variant="contained" color="success" startIcon={<FitnessCenterIcon/>}>
+            +1 ficha
+        </Button>
+      </Box>
+      <Box hidden={currentTab !== 1}>
+        <div className={classes.mainDiv}>
+          <Typography variant="h1" component="h1">
+            {fichas}
+          </Typography>
+          <Typography>
+            Fichas
+          </Typography>          
+        </div> 
+        <Button onClick={decreaseFichas} className={classes.spendCoinButton} size="large" variant="contained" color="secondary" startIcon={<SportsEsportsIcon/>}>
+            -1 ficha
+        </Button>
+      </Box>
     </div>
   );
 }
